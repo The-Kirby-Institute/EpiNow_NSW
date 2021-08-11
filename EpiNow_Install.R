7742f6f970c73ef116235e94df5ffed32427168e
leftygray <leftygray@gmail.com>
2021-08-12 09:14:32 +1000

    Initiate repository

---
+++ EpiNow_Install.R
@@ -0,0 +1,18 @@
+# This script is used for updating the EpiNow2 package and other required
+# packages to latest stable version. 
+
+install.packages("drat")
+drat:::add("epiforecasts")
+
+# EpiNow stuff
+# install.packages("EpiNow") # Deprecated replaced by EpiNow2 
+install.packages("EpiNow2")
+# install.packages("NCoVUtils") # Deprecated replaced by 
+install.packages("EpiSoon")
+#install.packages("NCoVUtils") # Deprecated replaced by covidregionaldata
+install.packages("covidregionaldata")
+
+# Other packages used
+install.packages("tidyverse")
+install.packages("lubridate")
+
