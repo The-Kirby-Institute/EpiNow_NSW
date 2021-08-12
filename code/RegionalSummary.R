25149b16e572bb6e65aa274f1363d5c247bd0aa6
leftygray <leftygray@gmail.com>
2021-08-12 15:12:51 +1000

    Added a regional summary output producing function

---
+++ code/RegionalSummary.R
@@ -0,0 +1,14 @@
+#' Function to produce regional summary results
+#' 
+RegionalSummary <- function(output, folder) {
+    
+    results_dir = file.path("results", folder)
+    target_date = "latest"
+    summary_dir =  file.path(results_dir, "regional-summary", "latest")
+    
+    EpiNow2::regional_summary(
+      regional_output = output$regional,
+      reported_cases = output$summary$reported_cases,
+      summary_dir = summary_dir,
+      target_date = target_date)
+  }
