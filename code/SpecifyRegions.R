2a5f0d452c00ab264c4a3c9c988d5e10f468acbf
leftygray <leftygray@gmail.com>
2021-08-12 12:49:57 +1000

    Fixed up regional summary and estimates

--- code/SpecifyRegions.R
+++ code/SpecifyRegions.R
@@ -1,20 +1,29 @@
-#' Select specific regions for EpiNow estimates
+#' Select specific regions for EpiNow estimates and corresponding results
+#' folder.
 #' 
-#' 
-SpecifyRegions <- function(option = "all") {
+SpecifyRegions <- function(level, option = "all") {
+  
+  if (level %in% c("NSW", "Sydney")) {
+    option == "all"
+  }
   
   if (option == "all") {
     regionList <- "all"
+    regionFolder <- level
   } else if (option == "KeyLGAs") {
     regionList <- c("Campbelltown (C) (NSW)", "Blacktown (C)",
       "Fairfield (C)", "Canterbury-Bankstown (A)", "Liverpool (C)",
       "Georges River (A)", "Parramatta (C)", "Cumberland (A)",
       "Penrith (C)")
+    regionFolder <- "LGAs-concern"
+  } else if (option == "TestLGAs") {
+    regionList <- c("Campbelltown (C) (NSW)", "Fairfield (C)")
+    regionFolder <- "Test-LGAs"
   } else {
    stop("Uknown regions option") 
   }
   
-  return(regionList)
+  return(list(regionList, regionFolder))
 }
   
   
