70ae9e52247e8f98ceece33c40759daa8df51f98
leftygray <leftygray@gmail.com>
2021-08-16 19:38:03 +1000

    Added script to calculate reporting delays

--- EpiNow_update_delay.R
+++ EpiNow_update_delay.R
@@ -11,11 +11,15 @@ require(data.table, quietly = TRUE)
 require(EpiNow2, quietly = TRUE)
 require(lubridate, quietly = TRUE)
 
+source("code/PlotOPtions.R")
+
 # Options -----------------------------------------------------------------
 delayData <- "local" # "international" or "local"
 
 # Local file to load if dataDelay = "local"
 localFile <- "data/delay_count_onsetdate.csv"
+
+saveFig <- TRUE
 
 # Get linelist ------------------------------------------------------------
 
@@ -38,9 +42,20 @@ if (delayData == "international") {
   
   # Fit the reporting delay -------------------------------------------------
   print("Generating international delay definitions ...")
-  delay_defs <- estimate_delay(delays, bootstraps = 100, samples = 1000)
+  delay_defs <- estimate_delay(delays, samples = 1000, bootstraps = 100)
   
   saveRDS(delay_defs, "data/international_delays.rds")
+  
+  if (saveFig) {
+    # Plot a figure of the delay distribution 
+    delayPlot <- ggplot() +
+      geom_histogram(aes(x = delays), binwidth = 1, alpha = 0.6) +
+      xlab("Reporting Delay") + ylab("Number of cases") + 
+      geom_vline(xintercept = 0, linetype = "dashed") +
+      PlotOptions()
+    
+    ggsave("data/international_delay_histogram.png" , plot = delayPlot)
+  }
   
 } else if (delayData == "local") {
   
@@ -50,12 +65,24 @@ if (delayData == "international") {
   
   data <- read_csv(localFile)
   #Get the delay distribution for all regions:
-  print("Generating loacl delay definitions ...")
-  delay_defs <- estimate_delay(data$delay, bootstraps = 100, 
-    samples = 1000)
+  print("Generating local delay definitions ...")
+  delay_defs <- estimate_delay(data$delay, max_value = 21, bootstraps = 1)
   
   saveRDS(delay_defs, "data/local_delays.rds")
+  
+  if (saveFig) {
+    # Plot a figure of the delay distribution 
+    delayPlot <- ggplot(data = data, aes( x = delay)) +
+      geom_histogram(binwidth = 1, alpha = 0.6) +
+      xlab("Reporting Delay") + ylab("Number of cases") + 
+      geom_vline(xintercept = 0, linetype = "dashed") +
+      PlotOptions()
+    
+    ggsave("data/local_delay_histogram.png" , plot = delayPlot)
+  }
   
 } else {
   stop("Unknown delay data option")
 }
+
+
